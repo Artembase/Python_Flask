6b1ac2c87ed7234a7f10e83bca3301670f35e04d
Artembase <38274507+Artembase@users.noreply.github.com>
2023-10-04 23:43:08 +0300

    feat: init database

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask,render_template, url_for
-
+from datetime import datetime
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), primary_key=False)
+    intro = db.Column(db.String(300), primary_key=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
 
 
 @app.route('/')
