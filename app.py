f2b8b9dbc6daaa486a63b36fae7e8e8577b10ea0
Artembase <38274507+Artembase@users.noreply.github.com>
2023-10-05 18:37:26 +0300

    refactor: db for expenses

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog1.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expenses.db'
 db = SQLAlchemy(app)
 
-#init database
+
+# init database
 # from app import app, db
 # app.app_context().push()
 # db.create_all()
@@ -14,9 +15,13 @@ db = SQLAlchemy(app)
 
 class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), primary_key=False, default='')
-    intro = db.Column(db.String(300), primary_key=False, default='')
-    text = db.Column(db.Text, nullable=False, default='')
+    name = db.Column(db.String(100), primary_key=False)
+    index = db.Column(db.String(100), primary_key=False, default='')
+    quantity = db.Column(db.Integer)
+    cost = db.Column(db.Integer, nullable=False)
+    # index_sal = db.Column(db.String(100), primary_key=False)
+    # sum_sal = db.Column(db.String(100), primary_key=False)
+    # name_sal = db.Column(db.Integer)
     date = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
@@ -43,11 +48,12 @@ def posts():
 @app.route('/create-article', methods=['POST', 'GET'])
 def create_article():
     if request.method == 'POST':
-        title = request.form['title']
-        intro = request.form['intro']
-        text = request.form['text']
+        name = request.form['name']
+        index = request.form['index']
+        quantity = request.form['quantity']
+        cost = request.form['cost']
 
-        article = Article(title=title, intro=intro, text=text)
+        article = Article(name=name, index=index, quantity=quantity, cost=cost)
 
         try:
             db.session.add(article)
@@ -55,6 +61,7 @@ def create_article():
             return redirect('/')
         except:
             return 'ПЫри добавление статьи произошла ошибка'
+
     else:
         return render_template("create-article.html")
 
