ea54474c1ffcfaa3c9264aa40c207e52bdce97b4
Artembase <38274507+Artembase@users.noreply.github.com>
2023-10-06 20:48:23 +0300

    refactor: db. Add income DB.

--- app.py
+++ app.py
@@ -19,13 +19,21 @@ class Article(db.Model):
     index = db.Column(db.String(100), primary_key=False, default='')
     quantity = db.Column(db.Integer)
     cost = db.Column(db.Integer, nullable=False)
-    # index_sal = db.Column(db.String(100), primary_key=False)
-    # sum_sal = db.Column(db.String(100), primary_key=False)
-    # name_sal = db.Column(db.Integer)
     date = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<Article %r>' % self.id
+
+
+class Income(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    index_sal = db.Column(db.Integer, primary_key=False)
+    sum_sal = db.Column(db.Integer, primary_key=False)
+    name_sal = db.Column(db.String(100))
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Income %r>' % self.id
 
 
 @app.route('/')
@@ -45,6 +53,12 @@ def posts():
     return render_template("posts.html", articles=articles)
 
 
+@app.route('/incomes')
+def posts2():
+    income = Income.query.order_by(Income.date.desc()).all()
+    return render_template("incomes.html", income=income)
+
+
 @app.route('/create-article', methods=['POST', 'GET'])
 def create_article():
     if request.method == 'POST':
@@ -60,10 +74,30 @@ def create_article():
             db.session.commit()
             return redirect('/')
         except:
-            return 'ПЫри добавление статьи произошла ошибка'
+            return 'ПЫри добавление расходов произошла ошибка'
 
     else:
         return render_template("create-article.html")
+
+
+@app.route('/income', methods=['POST', 'GET'])
+def income1():
+    if request.method == 'POST':
+        index_sal = request.form['index_sal']
+        sum_sal = request.form['sum_sal']
+        name_sal = request.form['name_sal']
+
+        income = Income(index_sal=index_sal, sum_sal=sum_sal, name_sal=name_sal)
+
+        try:
+            db.session.add(income)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'ПЫри добавление дохода произошла ошибка'
+
+    else:
+        return render_template("income.html")
 
 
 if __name__ == "__main__":
